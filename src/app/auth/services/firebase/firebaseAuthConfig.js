b9c939ebf24a59dd364882650db63bd0d3664712
Africanshops <africanshops@localhost.localdomain>
2024-09-06 16:27:18 +0100

    v3 merchant first commit

--- src/app/auth/services/firebase/firebaseAuthConfig.js
+++ src/app/auth/services/firebase/firebaseAuthConfig.js
@@ -1,9 +1,25 @@
+// import { initializeApp } from 'firebase/app';
+// import { getAnalytics } from 'firebase/analytics';
+
+
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	// apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	// authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
 	databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID
+	// projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	// storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	// messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+
+
+	apiKey: 'AIzaSyAr8p5srvGfuBIqqtsUhFQwgFjtSdSuvzg',
+	authDomain: 'africanshopsecommerce.firebaseapp.com',
+	projectId: 'africanshopsecommerce',
+	storageBucket: 'africanshopsecommerce.appspot.com',
+	messagingSenderId: '248778875159',
+	appId: '1:248778875159:web:fca2c9daf5f2e7e7679752',
+	measurementId: 'G-WJH9CH067R',
 };
+
+
+
 export default firebaseConfig;
--- src/app/auth/services/firebase/initializeFirebase.jsx
+++ src/app/auth/services/firebase/initializeFirebase.jsx
@@ -1,11 +1,34 @@
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/database';
+// import firebaseConfig from './firebaseAuthConfig';
+// /**
+//  * Initialize Firebase
+//  */
+// export const firebaseApp = firebase.initializeApp(firebaseConfig);
+// /**
+//  * Firebase App initialization check
+//  */
+// let initialized = false;
+// try {
+// 	firebase?.auth();
+// 	initialized = true;
+// } catch (e) {
+// 	console.error(e);
+// }
+// export const firebaseInitialized = initialized;
+
+
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/database';
 import firebaseConfig from './firebaseAuthConfig';
+import { getAnalytics } from 'firebase/analytics';
 /**
  * Initialize Firebase
  */
 export const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const analytics = getAnalytics(firebaseConfig);
 /**
  * Firebase App initialization check
  */
@@ -17,3 +40,4 @@ try {
 	console.error(e);
 }
 export const firebaseInitialized = initialized;
+
