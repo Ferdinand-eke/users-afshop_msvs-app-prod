be1c97200baff8559612927d027849eda578c30d
Ferdinand-eke <eke.benedictwitto@gmail.com>
2025-02-13 13:16:00 +0100

    Update: added carousel and tidied up homepage

--- src/app/main/zrootclient/buz-marketplace/shops/components/CategoryAndTradehub.jsx
+++ src/app/main/zrootclient/buz-marketplace/shops/components/CategoryAndTradehub.jsx
@@ -1,69 +1,52 @@
-import NavLinkAdapter from '@fuse/core/NavLinkAdapter';
-import { Typography } from '@mui/material';
-import useProductCats from 'app/configs/data/server-calls/product-categories/useProductCategories'
-import React from 'react'
-import { useParams } from 'react-router'
+import NavLinkAdapter from "@fuse/core/NavLinkAdapter";
+import { Typography } from "@mui/material";
+import useProductCats from "app/configs/data/server-calls/product-categories/useProductCategories";
+import React from "react";
+import { useParams } from "react-router";
 
 const CategoryAndTradehub = () => {
-    const routeParams = useParams();
-    const { id } = routeParams;
+  const routeParams = useParams();
+  const { id } = routeParams;
 
-    const {data:categories, isLoading} = useProductCats()
+  const { data: categories, isLoading } = useProductCats();
 
-
-  
-
-    let categoriesView;
-    if (!isLoading) {
-        if (categories?.data?.data && categories?.data?.data.length > 0) {
-            const items = categories?.data?.data.map((item) => (
-                <li key={item._id} className={item._id === id ? 'relative bg-orange-100 rounded-md hover:bg-orange-300 py-1 px-4 cursor-pointer'
-                 : 'relative bg-white rounded-md hover:bg-orange-500 py-1 px-4 cursor-pointer'}>
-                    <Typography
-                    //  href={`/afshopcategory/${item._id}`}
-                    component={NavLinkAdapter}
-                    to={`/marketplace/products/${item?._id}/by-category`}
-                     >
-                        {item.name}
-                    </Typography>
-                </li>
-            ));
-            categoriesView = <ul className="space-y-2 p-4">{items}</ul>;
-        } else {
-        }
+  let categoriesView;
+  if (!isLoading) {
+    if (categories?.data?.data && categories?.data?.data.length > 0) {
+      const items = categories?.data?.data.map((item) => (
+        <li
+          key={item._id}
+          className={
+            item._id === id
+              ? "relative bg-orange-100 rounded-md hover:bg-orange-300 py-1 px-4 cursor-pointer"
+              : "relative bg-white rounded-md hover:bg-orange-500 py-1 px-4 cursor-pointer"
+          }
+        >
+          <Typography
+            //  href={`/afshopcategory/${item._id}`}
+            component={NavLinkAdapter}
+            to={`/marketplace/products/${item?._id}/by-category`}
+          >
+            {item.name}
+          </Typography>
+        </li>
+      ));
+      categoriesView = <ul className="space-y-2 p-4">{items}</ul>;
     } else {
-        categoriesView = <p>Loading...</p>;
     }
+  } else {
+    categoriesView = <p>Loading...</p>;
+  }
 
   return (
-<>
-<h2 className="font-bold mb-4 p-4">CATEGORY</h2>
-                {/* <ul className="space-y-2 p-4">
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Computing</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Electronics</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Phones & Tablets</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Home & Office</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Automobile</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Computing</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Electronics</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Phones & Tablets</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Home & Office</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Automobile</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Computing</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Electronics</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Phones & Tablets</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Home & Office</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Automobile</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Computing</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Electronics</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Phones & Tablets</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Home & Office</li>
-                  <li className='relative bg-white rounded-md hover:bg-orange-100 py-1 px-4 cursor-pointer'>Automobile</li>
-                </ul> */}
+    <>
+      <div className="md:col-span-6 h-[300px]">
+        <h2 className="font-bold mb-4 p-4">CATEGORY</h2>
 
-                {categoriesView}
-</>
-  )
-}
+        {categoriesView}
+      </div>
+    </>
+  );
+};
 
-export default CategoryAndTradehub
+export default CategoryAndTradehub;
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import './i18n';
 import './styles/app-base.css';
 import './styles/app-components.css';
 import './styles/app-utilities.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { createRoot } from 'react-dom/client';
 import App from './app/App';
 // import * as serviceWorker from './serviceWorker';
